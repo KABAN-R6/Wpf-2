5386a0232f54ca6ee1a77e588432f45dd2dac8e7
is13126 <is13126@B206-2>
2023-03-18 12:48:11 +0700

    dssdgdsgsdg

--- Wpf№2/MainWindow.xaml.cs
+++ Wpf№2/MainWindow.xaml.cs
@@ -12,23 +12,51 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Drawing;
+
 
 namespace Wpf_2
 {
+
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        
         public MainWindow()
         {
             InitializeComponent();
+            
+            afas.FontSize = 10;
         }
 
         private void MenuItem_Click(object sender, RoutedEventArgs e)
         {
             MenuItem menuItem = (MenuItem)sender;
             MessageBox.Show(menuItem.Header.ToString());
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+           
+        }
+
+        private void afas_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+        private void rtbEditor_SelectionChanged(object sender,RoutedEventArgs e)
+        {
+            
+        }
+
+        private void afas_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            object temp = afas.Selection.GetPropertyValue(Inline.FontWeightProperty);
+            
         }
     }
 }
